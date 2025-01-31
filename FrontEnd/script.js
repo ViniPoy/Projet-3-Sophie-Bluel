8c11ee636a0b95409bd9de35f8e3c877f9b39eb8
ViniPoy <vinipoy@gmail.com>
2025-01-31 17:04:09 +0100

    Ajout de l'étape 1.2 du projet 3 (donc de l'étape 1 complète): Création de la régénération du DOM au clique sur les boutons de tri, ainsi qu'une légère modification des classes des boutons (et donc des sélécteurs css)

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -5,18 +5,23 @@ const works = await reponseWorks.json();
 
 //Récupération de la div dans laquelle implémenter les <figure>
 const divGallery = document.querySelector(".gallery");
-//Création et ajout de chaque <figure> dans la <div class="gallery">
-works.forEach((figure) => {
-    const figureElement = document.createElement("figure");
-    const imageElement = document.createElement("img");
-    imageElement.src = figure.imageUrl;
-    const figcaptionElement = document.createElement("figcaption");
-    figcaptionElement.innerText = figure.title;
-    //Rattachement des éléments
-    divGallery.appendChild(figureElement);
-    figureElement.appendChild(imageElement);
-    figureElement.appendChild(figcaptionElement);
-});
+//Création d'une fonction pour générer les éléments dans .gallery
+function genererFigure(works) {
+    //Création et ajout de chaque <figure> dans la <div class="gallery">
+    works.forEach((figure) => {
+        const figureElement = document.createElement("figure");
+        const imageElement = document.createElement("img");
+        imageElement.src = figure.imageUrl;
+        const figcaptionElement = document.createElement("figcaption");
+        figcaptionElement.innerText = figure.title;
+        //Rattachement des éléments
+        divGallery.appendChild(figureElement);
+        figureElement.appendChild(imageElement);
+        figureElement.appendChild(figcaptionElement);
+    });
+}
+//Appel de la fonction
+genererFigure(works);
 
 
 //Récupération de la section dans laquelle implémenter mes filtres
@@ -28,18 +33,43 @@ divFiltres.classList.add("filtres");
 divGallery.insertAdjacentElement("beforebegin", divFiltres);
 //Création et attachement des boutons
 const boutonTous = document.createElement("button");
-boutonTous.classList.add("btn-trier");
+boutonTous.classList.add("btn-filtrer");
 boutonTous.innerText = "Tous";
 divFiltres.appendChild(boutonTous);
 const boutonObjets = document.createElement("button");
-boutonObjets.classList.add("btn-trier");
+boutonObjets.classList.add("btn-filtrer");
 boutonObjets.innerText = "Objets";
 divFiltres.appendChild(boutonObjets);
 const boutonAppart = document.createElement("button");
-boutonAppart.classList.add("btn-trier");
+boutonAppart.classList.add("btn-filtrer");
 boutonAppart.innerText = "Appartements";
 divFiltres.appendChild(boutonAppart);
 const boutonHotels = document.createElement("button");
-boutonHotels.classList.add("btn-trier");
+boutonHotels.classList.add("btn-filtrer");
 boutonHotels.innerText = "Hotels & Restaurants";
 divFiltres.appendChild(boutonHotels);
+
+
+//Création des évènements suite aux cliques sur les diférents bouton et génération des pages filtrées
+boutonTous.addEventListener("click", function () {
+    divGallery.innerHTML = "";
+    genererFigure(works);
+})
+
+boutonObjets.addEventListener("click", function () {
+    const worksFiltres = works.filter(work => work.categoryId === 1);
+    divGallery.innerHTML = "";
+    genererFigure(worksFiltres);
+})
+
+boutonAppart.addEventListener("click", function () {
+    const worksFiltres = works.filter(work => work.categoryId === 2);
+    divGallery.innerHTML = "";
+    genererFigure(worksFiltres);
+})
+
+boutonHotels.addEventListener("click", function () {
+    const worksFiltres = works.filter(work => work.categoryId === 3);
+    divGallery.innerHTML = "";
+    genererFigure(worksFiltres);
+})
