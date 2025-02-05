c69266696a3f01cd0b69d05af21131aa557a5954
ViniPoy <vinipoy@gmail.com>
2025-02-05 14:33:36 +0100

    Ajout de l'étape 2.2 du projet 3 finie: la page chargée après authentification est modifiée pour correspondre à la maquette, avec insertion d'un cliquable pour ouvrir la modale (étape suivante).

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -84,13 +84,35 @@ const token = localStorage.getItem("userToken");
 
 if (token) {
     console.log("Utilisateur connecté, affichage des options admin.");
+    //Changement du bouton login en logout, quand on clique sur logout, on reste sur la page mais en tant que visiteur
     boutonLogin.innerText = "logout";
     boutonLogin.addEventListener("click", () => {
         window.location.href = "index.html";
         localStorage.removeItem("userToken"); 
     })
+    //On vire les boutons de filtres comme sur le figma
     divFiltres.innerHTML = "";
-    
+    //On créé le bandeau noir pour bien in diquer le mode admin
+    const divEdition = document.createElement("div");
+    divEdition.classList.add("mode-edition");
+    const header = document.querySelector("header");
+    header.insertAdjacentElement("beforebegin", divEdition);
+    divEdition.innerHTML = "<i class='fa-regular fa-pen-to-square'></i> Mode édtion";
+    //Création du lien vers la modale
+    const sectionPortfolio = document.getElementById("portfolio");
+    const titreProjet = document.querySelector("#portfolio h2");
+    const divEntete = document.createElement("div");
+    divEntete.classList.add("entete");
+    sectionPortfolio.insertBefore(divEntete, titreProjet);
+    divEntete.appendChild(titreProjet);
+    const lienModale = document.createElement("a")
+    lienModale.href = "#";
+    lienModale.innerHTML = "<i class='fa-regular fa-pen-to-square'></i> Modifier"
+    divEntete.appendChild(lienModale)
+    lienModale.addEventListener("click", (event) => {
+        event.preventDefault;
+        console.log("Modale ouverte");
+    })
 } else {
     console.log("Utilisateur non connecté, mode visiteur.");
 }
